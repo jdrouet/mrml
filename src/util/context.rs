673675befa3fda488dd04d0a47135c98bbb287b9
Jérémie Drouet <jeremie.drouet@gmail.com>
2020-05-19 12:04:12 +0200

    test: increate coverage on Context struct
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/util/context.rs
+++ src/util/context.rs
@@ -116,3 +116,50 @@ impl Context {
         self.index == self.siblings - 1
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Options;
+
+    #[test]
+    fn with_default_from() {
+        let origin = Context::default(Options::default());
+        let result = Context::default_from(&origin);
+        assert_eq!(origin.options().breakpoint, result.options().breakpoint);
+        assert_eq!(
+            origin.options().keep_comments,
+            result.options().keep_comments
+        );
+    }
+
+    #[test]
+    fn set_container_width() {
+        let mut ctx = Context::default(Options::default());
+        ctx.set_container_width(Some(Size::Percent(32.0)));
+        assert_eq!(ctx.container_width().unwrap().value(), 32.0);
+    }
+
+    #[test]
+    fn with_params() {
+        let mut ctx = Context::new(Options::default(), Some(Size::Percent(42.0)), 4, 2, 1);
+        assert_eq!(ctx.container_width().unwrap().value(), 42.0);
+        assert_eq!(ctx.siblings(), 4);
+        ctx.set_siblings(6);
+        assert_eq!(ctx.siblings(), 6);
+        assert_eq!(ctx.raw_siblings(), 2);
+        assert_eq!(ctx.non_raw_siblings(), 4);
+        ctx.set_raw_siblings(5);
+        assert_eq!(ctx.raw_siblings(), 5);
+        assert_eq!(ctx.non_raw_siblings(), 1);
+        assert_eq!(ctx.index(), 1);
+        assert_eq!(ctx.is_first(), false);
+        assert_eq!(ctx.is_last(), false);
+        ctx.set_index(0);
+        assert_eq!(ctx.is_first(), true);
+        assert_eq!(ctx.is_last(), false);
+        ctx.set_index(5);
+        assert_eq!(ctx.is_first(), false);
+        assert_eq!(ctx.is_last(), true);
+    }
+}
