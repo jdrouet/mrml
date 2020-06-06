7f519c25f437a17f7f768896e6fa063a285737f4
Jérémie Drouet <jeremie.drouet@gmail.com>
2020-06-06 09:09:37 +0200

    mj-divider: use tag builder
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/util/condition.rs
+++ src/util/condition.rs
@@ -4,3 +4,7 @@ pub const END_CONDITIONAL_TAG: &str = "<![endif]-->";
 pub const START_NEGATION_CONDITIONAL_TAG: &str = "<!--[if !mso | IE]><!-->";
 pub const START_MSO_NEGATION_CONDITIONAL_TAG: &str = "<!--[if !mso><!-->";
 pub const END_NEGATION_CONDITIONAL_TAG: &str = "<!--<![endif]-->";
+
+pub fn conditional_tag(input: String) -> String {
+    format!("{}{}{}", START_CONDITIONAL_TAG, input, END_CONDITIONAL_TAG)
+}
