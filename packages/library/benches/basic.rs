46463f77a35a14c5d79bef9cc9f388ad10885eb6
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-02-05 20:25:01 +0100

    refactor(library): rewording bench
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/library/benches/basic.rs
+++ packages/library/benches/basic.rs
@@ -1,13 +1,15 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use mrml;
 
-fn render(input: &str) {
+fn render_default_options(input: &str) {
     mrml::to_html(input, mrml::Options::default()).unwrap();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let data = include_str!("../resources/compare/success/mj-button.mjml");
-    c.bench_function("render button", |b| b.iter(|| render(black_box(data))));
+    c.bench_function("render button", |b| {
+        b.iter(|| render_default_options(black_box(data)))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
