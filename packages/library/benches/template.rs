eb78adf1d231a89b098bbe3bbdc5a17e2868b554
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-02-05 19:03:44 +0100

    test(library): bench render without comments
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/library/benches/template.rs
+++ packages/library/benches/template.rs
@@ -1,14 +1,27 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use mrml;
 
-fn render(input: &str) {
+fn render_default_options(input: &str) {
     mrml::to_html(input, mrml::Options::default()).unwrap();
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let data = include_str!("../resources/template/amario.mjml");
-    c.bench_function("amario", |b| b.iter(|| render(black_box(data))));
+fn render_without_comments(input: &str) {
+    let opts = mrml::Options {
+        keep_comments: false,
+        ..mrml::Options::default()
+    };
+    mrml::to_html(input, opts).unwrap();
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn amario_benchmark(c: &mut Criterion) {
+    let data = include_str!("../resources/template/amario.mjml");
+    c.bench_function("amario", |b| {
+        b.iter(|| render_default_options(black_box(data)))
+    });
+    c.bench_function("amario without comments", |b| {
+        b.iter(|| render_without_comments(black_box(data)))
+    });
+}
+
+criterion_group!(benches, amario_benchmark);
 criterion_main!(benches);
