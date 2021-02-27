d31609052fa5e347247d3946bc1011a5b7334a67
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-02-27 09:02:42 +0100

    refacto(library): use sort_by_key in header
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/library/src/util/condition.rs
+++ packages/library/src/util/condition.rs
@@ -6,17 +6,17 @@ pub const START_MSO_NEGATION_CONDITIONAL_TAG: &str = "<!--[if !mso><!-->";
 pub const END_NEGATION_CONDITIONAL_TAG: &str = "<!--<![endif]-->";
 
 pub fn conditional_tag(input: String) -> String {
-    START_CONDITIONAL_TAG.to_string() + &input + &END_CONDITIONAL_TAG
+    START_CONDITIONAL_TAG.to_string() + &input + END_CONDITIONAL_TAG
 }
 
 pub fn negation_conditional_tag(input: String) -> String {
-    START_NEGATION_CONDITIONAL_TAG.to_string() + &input + &END_NEGATION_CONDITIONAL_TAG
+    START_NEGATION_CONDITIONAL_TAG.to_string() + &input + END_NEGATION_CONDITIONAL_TAG
 }
 
 pub fn mso_conditional_tag(input: String) -> String {
-    START_MSO_CONDITIONAL_TAG.to_string() + &input + &END_CONDITIONAL_TAG
+    START_MSO_CONDITIONAL_TAG.to_string() + &input + END_CONDITIONAL_TAG
 }
 
 pub fn mso_negation_conditional_tag(input: String) -> String {
-    START_MSO_NEGATION_CONDITIONAL_TAG.to_string() + &input + &END_NEGATION_CONDITIONAL_TAG
+    START_MSO_NEGATION_CONDITIONAL_TAG.to_string() + &input + END_NEGATION_CONDITIONAL_TAG
 }
