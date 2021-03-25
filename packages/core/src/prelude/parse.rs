d44f900ffb420fc028a5cdf5b1e8b8a186fa56ce
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-03-25 07:32:11 +0100

    feat(core): move macros to prelude files
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/core/src/prelude/parse.rs
+++ packages/core/src/prelude/parse.rs
@@ -1,5 +1,59 @@
 use xmlparser::{StrSpan, Token, Tokenizer};
 
+#[macro_export]
+macro_rules! parse_attribute {
+    () => {
+        fn parse_attribute<'a>(
+            &mut self,
+            name: xmlparser::StrSpan<'a>,
+            value: xmlparser::StrSpan<'a>,
+        ) -> Result<(), Error> {
+            self.0
+                .attributes
+                .insert(name.to_string(), value.to_string());
+            Ok(())
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! parse_child {
+    ($child_parser:ident) => {
+        fn parse_child_element<'a>(
+            &mut self,
+            tag: xmlparser::StrSpan<'a>,
+            tokenizer: &mut xmlparser::Tokenizer<'a>,
+        ) -> Result<(), Error> {
+            self.0.children.push($child_parser::parse(tag, tokenizer)?);
+            Ok(())
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! parse_comment {
+    () => {
+        fn parse_child_comment(&mut self, value: xmlparser::StrSpan) -> Result<(), Error> {
+            self.0
+                .children
+                .push(crate::comment::Comment::from(value.as_str()).into());
+            Ok(())
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! parse_text {
+    () => {
+        fn parse_child_text(&mut self, value: xmlparser::StrSpan) -> Result<(), Error> {
+            self.0
+                .children
+                .push(crate::text::Text::from(value.as_str()).into());
+            Ok(())
+        }
+    };
+}
+
 #[derive(Debug)]
 pub enum Error {
     UnexpectedAttribute(usize),
