24da448ae41c5667de5c1e82cef5d9f3393a7768
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-04-16 07:45:00 +0200

    test(mj-title): increate parse code coverage
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/mrml-core/src/mj_title/parse.rs
+++ packages/mrml-core/src/mj_title/parse.rs
@@ -23,3 +23,14 @@ impl Parsable for MJTitle {
         MJTitleParser::default().parse(tokenizer)?.build()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn success() {
+        let res = crate::mjml::MJML::parse(
+            r#"<mjml><mj-head><mj-title>Hello World!</mj-title></mj-head></mjml>"#,
+        );
+        assert!(res.is_ok());
+    }
+}
