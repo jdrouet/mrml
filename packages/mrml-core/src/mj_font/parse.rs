6cd867f916b8ddb43d74a7d122b9796ee097b90f
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-04-16 07:45:00 +0200

    test(mj-font): increate parse code coverage
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/mrml-core/src/mj_font/parse.rs
+++ packages/mrml-core/src/mj_font/parse.rs
@@ -32,3 +32,24 @@ impl Parsable for MJFont {
         MJFontParser::default().parse(tokenizer)?.build()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::mjml::MJML;
+
+    #[test]
+    fn success() {
+        assert!(MJML::parse(
+            r#"<mjml><mj-head><mj-font name="Comic" href="https://jolimail.io" /></mj-head></mjml>"#
+        )
+        .is_ok());
+    }
+
+    #[test]
+    fn unexpected_attribute() {
+        assert!(
+            MJML::parse(r#"<mjml><mj-head><mj-font unknown="whatever" /></mj-head></mjml>"#)
+                .is_err()
+        );
+    }
+}
