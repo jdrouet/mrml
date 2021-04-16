ec7fdbfd251d7550a3d3ba238dfd3a31b3e8c45f
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-04-16 21:10:50 +0200

    feat(wasm): add toJson, toMjml and validate
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/mrml-wasm/tests/wasm.rs
+++ packages/mrml-wasm/tests/wasm.rs
@@ -32,3 +32,37 @@ fn to_html_with_options_raise() {
     let result = mrml_wasm::to_html_with_options("<mjml", &opts);
     assert!(result.is_err());
 }
+
+#[wasm_bindgen_test]
+fn validate_raise() {
+    assert!(mrml_wasm::validate("<mjml").is_err());
+}
+
+#[wasm_bindgen_test]
+fn validate_pass() {
+    assert!(mrml_wasm::validate("<mjml></mjml>").is_ok());
+}
+
+#[wasm_bindgen_test]
+fn to_json() {
+    assert_eq!(
+        mrml_wasm::to_json(r#"<mjml></mjml>"#, false).unwrap(),
+        r#"{"type":"mjml"}"#
+    );
+    assert_eq!(
+        mrml_wasm::to_json(r#"<mjml></mjml>"#, true).unwrap(),
+        "{\n  \"type\": \"mjml\"\n}"
+    );
+}
+
+#[wasm_bindgen_test]
+fn to_mjml() {
+    assert_eq!(
+        mrml_wasm::to_mjml(r#"{"type":"mjml"}"#, false).unwrap(),
+        r#"<mjml></mjml>"#
+    );
+    assert_eq!(
+        mrml_wasm::to_mjml(r#"{"type":"mjml"}"#, true).unwrap(),
+        "<mjml>\n</mjml>\n"
+    );
+}
