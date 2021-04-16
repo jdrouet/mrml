dec2d3366a50c089e04a0c9f509b3b8a73b26a41
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-04-16 07:45:00 +0200

    test(mj-preview): increate parse code coverage
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/mrml-core/src/mj_preview/parse.rs
+++ packages/mrml-core/src/mj_preview/parse.rs
@@ -23,3 +23,14 @@ impl Parsable for MJPreview {
         MJPreviewParser::default().parse(tokenizer)?.build()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn success() {
+        let res = crate::mjml::MJML::parse(
+            r#"<mjml><mj-head><mj-preview>Hello World!</mj-preview></mj-head></mjml>"#,
+        );
+        assert!(res.is_ok());
+    }
+}
