ef9484d49198171d4b62ba2c1a1c11bcc3432356
Jérémie Drouet <jeremie.drouet@gmail.com>
2021-04-16 07:45:00 +0200

    test(mj-carousel): increase coverage for parse and print
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/mrml-core/src/mj_carousel/print.rs
+++ packages/mrml-core/src/mj_carousel/print.rs
@@ -12,4 +12,18 @@ mod tests {
         let item = crate::mj_carousel::MJCarousel::default();
         assert_eq!("<mj-carousel></mj-carousel>", item.dense_print());
     }
+
+    #[test]
+    fn with_images() {
+        let json = r#"<mjml>
+  <mj-body>
+    <mj-carousel>
+      <mj-carousel-image />
+    </mj-carousel>
+  </mj-body>
+</mjml>
+"#;
+        let root = crate::mjml::MJML::parse(json).unwrap();
+        assert_eq!(json, root.pretty_print());
+    }
 }
