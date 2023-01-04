75b0c630e525a7281584529e0ff5924c7a9f65a3
Jérémie Drouet <jeremie.drouet@gmail.com>
2023-01-04 21:16:19 +0000

    chore: remove migrated packages
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/mrml-core/src/helper/sort.rs
+++
@@ -1,5 +0,0 @@
-use std::cmp::Ordering;
-
-pub fn sort_by_key<'r, 's, V>(a: &'r (&String, V), b: &'s (&String, V)) -> Ordering {
-    a.0.partial_cmp(b.0).unwrap()
-}
--- packages/mrml-core/src/prelude/mod.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(feature = "parse")]
-pub mod parse;
-#[cfg(feature = "print")]
-pub mod print;
-#[cfg(feature = "render")]
-pub mod render;
-
-pub mod hash;
