82ee5af980b07e6f26a39fd5dba04743a105afb8
Jérémie Drouet <jeremie.drouet@gmail.com>
2023-07-30 12:13:50 +0000

    refactor(mrml-core): more tests
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/mrml-core/src/mj_navbar_link/parse.rs
+++ packages/mrml-core/src/mj_navbar_link/parse.rs
@@ -24,3 +24,37 @@ impl<'a> ElementParser<'a, MjNavbarLink> for MrmlParser<'a> {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::MjNavbarLink;
+    use crate::prelude::parser::MrmlParser;
+
+    macro_rules! assert_success {
+        ($title:ident, $template:expr) => {
+            #[test]
+            fn $title() {
+                let _: MjNavbarLink = MrmlParser::new($template, Default::default())
+                    .parse_root()
+                    .unwrap();
+            }
+        };
+    }
+
+    assert_success!(should_handle_empty_children, "<mj-navbar-link />");
+
+    assert_success!(
+        should_handle_comments,
+        "<mj-navbar-link><!-- comment --></mj-navbar-link>"
+    );
+
+    assert_success!(
+        should_work_with_text,
+        "<mj-navbar-link>Hello</mj-navbar-link>"
+    );
+
+    assert_success!(
+        should_work_with_other_element,
+        "<mj-navbar-link><span /></mj-navbar-link>"
+    );
+}
