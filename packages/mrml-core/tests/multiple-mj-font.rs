12e7a32c4134198a2a6c3bef60e523ba47fb94b7
Jérémie Drouet <jdrouet@users.noreply.github.com>
2024-03-04 21:33:31 +0100

    fix(mrml-core): ensure fonts are rendered once (#383)
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

---
+++ packages/mrml-core/tests/multiple-mj-font.rs
@@ -0,0 +1,23 @@
+#![cfg(feature = "parse")]
+
+use mrml::prelude::render::RenderOptions;
+
+#[test]
+fn should_have_a_single_roboto_font_imported() {
+    let parsed = mrml::parse(r#"<mjml>
+  <mj-head>
+    <mj-font name="Roboto" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" />
+    </mj-head>
+    <mj-body>
+    <mj-section>
+        <mj-column>
+        <mj-text font-family="Roboto, Arial">
+            Hello World!
+        </mj-text>
+        </mj-column>
+    </mj-section>
+    </mj-body>
+</mjml>"#).unwrap();
+    let rendered = parsed.render(&RenderOptions::default()).unwrap();
+    assert!(!rendered.contains("Roboto:300,400,500,700"));
+}
