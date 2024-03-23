ccebd23549f937464303d65c7bd45f7c6379026e
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-03-23 18:56:06 +0100

    example(demo): prepare for release build
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- examples/demo/vite.config.ts
+++ examples/demo/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [react(), wasm(), topLevelAwait()],
 })
