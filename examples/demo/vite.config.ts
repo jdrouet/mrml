d99ee6a9fcaa6d9b5eee5000666bd45986c417c0
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-03-23 18:53:46 +0100

    example(demo): use mrml-wasm with vite
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- examples/demo/vite.config.ts
+++ examples/demo/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import wasm from "vite-plugin-wasm";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm()],
 })
