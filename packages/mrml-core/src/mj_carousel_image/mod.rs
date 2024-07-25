6a0acee36564ffaf645619b947d2667511ddb568
Jérémie Drouet <jeremie.drouet@gmail.com>
2024-07-25 07:10:35 +0200

    refactor: make mj-carousel-image an abstract component
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- packages/mrml-core/src/mj_carousel_image/mod.rs
+++ packages/mrml-core/src/mj_carousel_image/mod.rs
@@ -1,19 +1,22 @@
 #[cfg(feature = "json")]
 mod json;
-#[cfg(feature = "parse")]
-mod parse;
 #[cfg(feature = "print")]
 mod print;
 #[cfg(feature = "render")]
 mod render;
 
-use crate::prelude::hash::Map;
+use std::marker::PhantomData;
+
+use crate::prelude::{hash::Map, Component, StaticTag};
 
 pub const NAME: &str = "mj-carousel-image";
 
-#[derive(Clone, Debug, Default)]
-#[cfg_attr(feature = "json", derive(mrml_json_macros::MrmlJsonComponent))]
-#[cfg_attr(feature = "json", mrml_json(tag = "NAME"))]
-pub struct MjCarouselImage {
-    pub attributes: Map<String, String>,
+pub struct MjCarouselImageTag;
+
+impl StaticTag for MjCarouselImageTag {
+    fn static_tag() -> &'static str {
+        NAME
+    }
 }
+
+pub type MjCarouselImage = Component<PhantomData<MjCarouselImageTag>, Map<String, String>, ()>;
