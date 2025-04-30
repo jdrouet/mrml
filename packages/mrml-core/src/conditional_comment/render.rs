f63bfbbdf568a50694daf17e22babc34f8d61faa
Raúl Nozal <rnoz.commits@gmail.com>
2025-04-30 11:29:27 +0100

    test: another render test to access content / as_ref
    Signed-off-by: Raúl Nozal <rnoz.commits@gmail.com>

--- packages/mrml-core/src/conditional_comment/render.rs
+++ packages/mrml-core/src/conditional_comment/render.rs
@@ -23,8 +23,10 @@ impl<'render, 'root: 'render> Renderable<'render, 'root> for ConditionalComment 
 
 #[cfg(all(test, feature = "parse"))]
 mod tests {
+    use crate::conditional_comment::ConditionalComment;
+    use crate::mj_head::MjHead;
     use crate::mjml::Mjml;
-    use crate::prelude::render::RenderOptions;
+    use crate::prelude::render::{Header, RenderContext, RenderCursor, RenderOptions, Renderable};
 
     #[test]
     fn render_fails_without_mj_raw() {
@@ -57,5 +59,24 @@ mod tests {
         let root = Mjml::parse(r#"<mjml><mj-body><mj-raw><!--[if mso]><!-- Comment --><span>SpanContent</span><![endif]--></mj-raw></mj-body></mjml>"#).unwrap();
         let result = root.element.render(&opts).unwrap();
         assert!(result.contains("<!--[if mso]><span>SpanContent</span><![endif]-->"));
+    }
+
+    #[test]
+    fn render_context() {
+        let opts = RenderOptions::default();
+        let mj_head = Some(MjHead::default());
+        let header = Header::new(mj_head.as_ref(), None);
+        let context = RenderContext::new(&opts, header);
+
+        let content = "<!--[if mso]><span>Test Content</span><![endif]-->";
+        let elt = ConditionalComment::from(content);
+        let renderer = elt.renderer(&context);
+
+        assert!(std::ptr::eq(renderer.context(), &context));
+
+        let mut cursor = RenderCursor::default();
+        renderer.render(&mut cursor).unwrap();
+
+        assert_eq!(cursor.buffer.as_ref(), content);
     }
 }
