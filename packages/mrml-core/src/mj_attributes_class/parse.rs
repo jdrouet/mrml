dfa3347b65675e195de412435ad6dc0d3e57abc3
Jeremie Drouet <jeremie.drouet@gmail.com>
2026-09-12 18:55:13 +0200

    test(mrml-core): pin attribute order when mj-class drops its name
    Signed-off-by: Jeremie Drouet <jeremie.drouet@gmail.com>

--- packages/mrml-core/src/mj_attributes_class/parse.rs
+++ packages/mrml-core/src/mj_attributes_class/parse.rs
@@ -72,4 +72,17 @@ mod tests {
         r#"<mj-class name="div" color="red"><whatever>"#,
         "UnexpectedToken { origin: Root, position: Span { start: 33, end: 42 } }"
     );
+
+    // pins shift_remove (order-preserving) over remove (swap-remove)
+    #[test]
+    fn removing_name_preserves_order_of_other_attributes() {
+        let opts = crate::prelude::parser::ParserOptions::default();
+        let parser = crate::prelude::parser::MrmlParser::new(&opts);
+        let mut cursor = crate::prelude::parser::MrmlCursor::new(
+            r#"<mj-class name="whatever" color="red" font-size="12px" padding="10px" />"#,
+        );
+        let elt: MjAttributesClass = parser.parse_root(&mut cursor).unwrap();
+        let keys: Vec<&str> = elt.attributes.others.keys().map(String::as_str).collect();
+        assert_eq!(keys, vec!["color", "font-size", "padding"]);
+    }
 }
